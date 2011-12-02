7ca06c2b8f374044ae843aa15ba9f5fd864c1b7a
Jefferson Almeida <jra@cin.ufpe.br>
2011-12-02 11:08:26 -0300

    construct the ui

--- src/jefferson/hello/app/HelloWorldActivity.java
+++ src/jefferson/hello/app/HelloWorldActivity.java
@@ -2,12 +2,20 @@ package jefferson.hello.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelloWorldActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        // Criacao da View
+        TextView texto = new TextView(this);
+        texto.setText("Hello Android !");
+        
+        // configura o conteudo de visualizacao
+        setContentView(texto);
+        
     }
 }
